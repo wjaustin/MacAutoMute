1da82c8b3f675817a4780a90cde201ab45fb663c
wjaustin <wjaustin@oakland.edu>
2017-02-24 15:12:32 -0500

    Soften Uninstall Script
    * The uninstall script now removes the file from /Library/Management and
    then checks to make sure the directory is then empty before removing it.
    It will not remove the directory if another file exsists within the
    directory.

--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,8 @@
 # Removes files and directories created by install.sh
 rm /bin/audiodevice
 rm /Library/LaunchDaemons/com.autoMute.restart.plist
-rm -R /Library/Management
+rm /Library/Management/autoMute.sh
+if [ ! "$(ls -A /Library/Management)" ]; then
+	rm -R /Library/Management/
+fi
 
